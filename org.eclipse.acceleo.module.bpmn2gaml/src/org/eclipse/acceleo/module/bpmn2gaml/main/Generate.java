021f8d102f6ad9b3d1992d1c38435055b1d6463c
ColinPBL <colinpoubel@orange.fr>
2021-07-26 15:58:10 +0200

    Updated buildpath to remove unused folders + changed Generate.java with fix

--- org.eclipse.acceleo.module.bpmn2gaml/src/org/eclipse/acceleo/module/bpmn2gaml/main/Generate.java
+++ org.eclipse.acceleo.module.bpmn2gaml/src/org/eclipse/acceleo/module/bpmn2gaml/main/Generate.java
@@ -22,7 +22,10 @@ import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.Monitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+
+import simplifiedbpmn.SimplifiedbpmnPackage;
 
 /**
  * Entry point of the 'Generate' generation module.
@@ -335,11 +338,12 @@ public class Generate extends AbstractAcceleoGenerator {
      * 
      * @param resourceSet
      *            The resource set which registry has to be updated.
-     * @generated
+     * @generated NOT
      */
     @Override
     public void registerPackages(ResourceSet resourceSet) {
         super.registerPackages(resourceSet);
+        EPackage.Registry.INSTANCE.put(SimplifiedbpmnPackage.eNS_URI, SimplifiedbpmnPackage.eINSTANCE);
         
         /*
          * If you want to change the content of this method, do NOT forget to change the "@generated"
